b47ca3bfe8e029c2f3488998b88a60015d5c00a1
przemek <przemekpapla@gmail.com>
2025-11-22 16:57:28 +0100

    bug

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,11 @@ import {routing} from '@/i18n/routing';
 import {hasLocale, NextIntlClientProvider} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {ReactNode} from 'react';
+import {setRequestLocale} from "next-intl/server";
+
+export function generateStaticParams() {
+    return routing.locales.map((locale) => ({locale}));
+}
 
 type Props = {
   children: ReactNode;
@@ -14,6 +19,9 @@ export default async function LocaleLayout({children, params}: Props) {
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
+
+  // Enable static rendering
+  // setRequestLocale(locale);
 
   return (
     <html lang={locale}>
